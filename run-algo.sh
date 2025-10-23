983a2842c638bb4b106ede0da58683d441fa701d
John Cruz <johnamcruz@gmail.com>
2025-10-23 09:13:33 -1000

    fix bug

---
+++ run-algo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 algoTrader.py --account 194638 --contract CON.F.US.EP.Z25 --size 1 --username johnamcruz --apikey fZYHCFdaJ1fzXVulwnUsAZrjOMAQdAOF9o6m9WOEIlI= --account 13210390
