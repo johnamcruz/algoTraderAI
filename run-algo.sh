8a3d974ba1f55d825f7ef66a96fb2485b28513d9
John Cruz <johnamcruz@gmail.com>
2025-10-23 09:44:29 -1000

    removed

--- run-algo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 algoTrader.py --account 194638 --contract CON.F.US.EP.Z25 --size 1 --username johnamcruz --apikey fZYHCFdaJ1fzXVulwnUsAZrjOMAQdAOF9o6m9WOEIlI= --account 13210390
