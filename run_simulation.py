2f9e7b6952fff38522fed9060568924e2cf5a2a3
John Cruz <johnamcruz@gmail.com>
2025-12-11 11:07:38 -1000

    added flag pattern

--- run_simulation.py
+++ run_simulation.py
@@ -8,4 +8,6 @@
 
 #python3 algoTrader.py --backtest --backtest_data data/NQ_continuous_3min.csv --contract CON.F.US.ENQ.Z25 --size 1 --entry_conf 0.4 --stop_pts 12.0 --target_pts 48.0 --model models/multihorizon_v3_6.onnx --scaler models/multihorizon_v3_6_scaler.pkl --strategy "multihorizon" --simulation-days 10
 
-python3 algoTrader.py --backtest --backtest_data data/ES_continuous_3min.csv --contract CON.F.US.EP.Z25 --size 1 --entry_conf 0.65 --stop_pts 8.0 --target_pts 32.0 --model models/ema_transformer_v1_2_fixed.onnx --scaler models/ema_transformer_v1_1_scaler.pkl --strategy "ema" --simulation-days 2
+#python3 algoTrader.py --backtest --backtest_data data/ES_continuous_3min.csv --contract CON.F.US.EP.Z25 --size 1 --entry_conf 0.65 --stop_pts 8.0 --target_pts 32.0 --model models/ema_transformer_v1_2_fixed.onnx --scaler models/ema_transformer_v1_1_scaler.pkl --strategy "ema" --simulation-days 2
+
+python3 algoTrader.py --backtest --backtest_data data/ES_continuous_3min.csv --contract CON.F.US.EP.Z25 --size 1 --entry_conf 0.65 --stop_pts 8.0 --target_pts 32.0 --model models/flag_pennant_v2_4.onnx --scaler models/flag_pennant_v2_4_scaler.pkl --strategy "flag" --simulation-days 30
