53d39ac6d49fd49ec6a4a9a1f636851e81a75b2d
John Cruz <johnamcruz@gmail.com>
2026-04-17 21:07:07 -1000

    updated sim script

--- run_simulation.py
+++ run_simulation.py
@@ -1,15 +1,3 @@
-#python3 algoTrader.py --backtest --backtest_data data/ES_continuous_3min.csv --contract CON.F.US.EP.Z25 --size 1 --entry_conf 0.75 --stop_pts 8.0 --target_pts 32.0 --model models/supertrend_pullback_v4.9.onnx --scaler models/supertrend_pullback_v4.9_scaler.pkl --strategy "supertrend3" --simulation-days 10
+#python3 algoTrader.py --backtest --backtest_data data/ES_continuous_5min.csv --contract CON.F.US.EP.Z25 --risk_amount 300 --entry_conf 0.70 --model models/cisd_ote_hybrid_v5_1.onnx --strategy "cisd-ote" --simulation-days 10
 
-#python3 algoTrader.py --backtest --backtest_data data/NQ_continuous_3min.csv --contract CON.F.US.ENQ.Z25 --size 1 --entry_conf 0.75 --stop_pts 40.0 --target_pts 120.0  --model models/supertrend_pullback_v4.9.onnx --scaler models/supertrend_pullback_v4.9_scaler.pkl --strategy "supertrend3" --simulation-days 5
-
-#python3 algoTrader.py --backtest --backtest_data data/ES_continuous_3min.csv --contract CON.F.US.EP.Z25 --size 1 --entry_conf 0.4 --stop_pts 8.0 --target_pts 32.0 --model models/multihorizon_v3_4.onnx --scaler models/multihorizon_v3_4_scaler.pkl --strategy "multihorizon" --simulation-days 30
-
-#python3 algoTrader.py --backtest --backtest_data data/ES_continuous_3min.csv --contract CON.F.US.EP.Z25 --size 1 --entry_conf 0.65 --stop_pts 8.0 --target_pts 32.0 --model models/multi_tf_transformer_v6_3_6.onnx --scaler models/multi_tf_transformer_v6_3_6_scaler.pkl --strategy "mtf_directional" --simulation-days 3
-
-#python3 algoTrader.py --backtest --backtest_data data/NQ_continuous_3min.csv --contract CON.F.US.ENQ.Z25 --size 1 --entry_conf 0.4 --stop_pts 12.0 --target_pts 48.0 --model models/multihorizon_v3_6.onnx --scaler models/multihorizon_v3_6_scaler.pkl --strategy "multihorizon" --simulation-days 10
-
-#python3 algoTrader.py --backtest --backtest_data data/ES_continuous_3min.csv --contract CON.F.US.EP.Z25 --size 1 --entry_conf 0.65 --stop_pts 8.0 --target_pts 32.0 --model models/ema_transformer_v1_2_fixed.onnx --scaler models/ema_transformer_v1_1_scaler.pkl --strategy "ema" --simulation-days 2
-
-#python3 algoTrader.py --backtest --backtest_data data/ES_continuous_3min.csv --contract CON.F.US.EP.Z25 --size 1 --entry_conf 0.65 --stop_pts 8.0 --target_pts 32.0 --model models/flag_pennant_v2_4.onnx --scaler models/flag_pennant_v2_4_scaler.pkl --strategy "flag" --simulation-days 10
-
-python3 algoTrader.py --backtest --backtest_data data/ES_continuous_5min.csv --contract CON.F.US.EP.Z25 --risk_amount 200 --entry_conf 0.70 --model models/cisd_ote_hybrid_v5_1.onnx --strategy "cisd-ote" --simulation-days 10 --debug
+python3 algoTrader.py --backtest --backtest_data data/NQ_continuous_5min.csv --contract CON.F.US.ENQ.Z25 --risk_amount 300 --entry_conf 0.70 --model models/cisd_ote_hybrid_v5_1.onnx --strategy "cisd-ote" --simulation-days 30
